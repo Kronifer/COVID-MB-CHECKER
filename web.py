573aa0a8d44065c25835f0397b82a1a5d39c802d
Kronifer <dillonr5@live.wsd1.org>
2021-04-06 19:36:45 +0000

    Began work on registration

--- web.py
+++ web.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymongo
+import os
+
+client = pymongo.MongoClient(
+    os.getenv("MKEY")
+)
+
+db = client.COVID_TRACKER
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
     return render_template("main.html")
+
+@app.route("/submit", methods=["GET"])
+def submit():
+    try:
+        email = request.args.get("email")
+    except:
+        return "No email submitted."
+    collection = db.emails
+    data = collection.find_one({"emails": []}, {"_id": 0})
+    return data
+
+    
+
+app.run(port=5000)
+    
